2a143bc633b238b949b990ee8e2e19defc0222a7
Phu Tang <phu.tanghong@2359media.com.vn>
2014-05-14 17:38:39 +0700

    [Finished #71288164]

--- MainSource/3min/src/com/threemin/app/HomeActivity.java
+++ MainSource/3min/src/com/threemin/app/HomeActivity.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -15,16 +15,17 @@ import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.threemin.adapter.CategoryAdapter;
 import com.threemin.fragment.BaseProductFragment;
 import com.threemin.fragment.ProductFragmentList;
 import com.threemin.model.CategoryModel;
-import com.threemin.uti.CommonUti;
-import com.threemin.webservice.AuthorizeWebservice;
 import com.threemin.webservice.CategoryWebservice;
 import com.threemins.R;
 
@@ -36,6 +37,9 @@ public class HomeActivity extends Activity {
 	DrawerLayout layout;
 	ActionBarDrawerToggle mDrawerToggle;
 	BaseProductFragment currentFragment;
+
+	ImageButton btn_browse, btn_search, btn_sell, btn_activity, btn_me;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -43,27 +47,46 @@ public class HomeActivity extends Activity {
 		setContentView(R.layout.activity_home);
 		mContext = this;
 		lvCategory = (ListView) findViewById(R.id.home_left_drawer);
-		layout=(DrawerLayout) findViewById(R.id.drawer_layout);
+		layout = (DrawerLayout) findViewById(R.id.drawer_layout);
 		initActionBar();
 		new InitCategory().execute();
 		productFragmentList = new ProductFragmentList();
-		getFragmentManager().beginTransaction().replace(R.id.content_fragment, productFragmentList).commit();
-		currentFragment=productFragmentList;
+		getFragmentManager().beginTransaction()
+				.replace(R.id.content_fragment, productFragmentList).commit();
+		currentFragment = productFragmentList;
 		lvCategory.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 					long arg3) {
-				CategoryModel categoryModel=(CategoryModel) lvCategory.getItemAtPosition(arg2);
+				CategoryModel categoryModel = (CategoryModel) lvCategory
+						.getItemAtPosition(arg2);
 				currentFragment.setCategoryData(categoryModel);
 				layout.closeDrawer(lvCategory);
 				getActionBar().setTitle(categoryModel.getName());
 			}
 		});
+		intitNavigation();
 	}
-	
 
-	private class InitCategory extends AsyncTask<Void, Void, List<CategoryModel>> {
+	private void intitNavigation() {
+		btn_browse = (ImageButton) findViewById(R.id.img_navigation_browse);
+		btn_search = (ImageButton) findViewById(R.id.img_navigation_search);
+		btn_me = (ImageButton) findViewById(R.id.img_navigation_me);
+		btn_sell = (ImageButton) findViewById(R.id.img_navigation_sell);
+		btn_activity = (ImageButton) findViewById(R.id.img_navigation_activity);
+
+		btn_browse.setOnClickListener(onNavigationClick());
+		btn_search.setOnClickListener(onNavigationClick());
+		btn_me.setOnClickListener(onNavigationClick());
+		btn_sell.setOnClickListener(onNavigationClick());
+		btn_sell.setOnClickListener(onNavigationClick());
+
+		btn_browse.setSelected(true);
+	}
+
+	private class InitCategory extends
+			AsyncTask<Void, Void, List<CategoryModel>> {
 		@Override
 		protected void onPreExecute() {
 			super.onPreExecute();
@@ -72,7 +95,8 @@ public class HomeActivity extends Activity {
 		@Override
 		protected List<CategoryModel> doInBackground(Void... arg0) {
 			try {
-				return CategoryWebservice.getInstance().getAllCategory(mContext);
+				return CategoryWebservice.getInstance()
+						.getAllCategory(mContext);
 			} catch (Exception e) {
 				e.printStackTrace();
 				return null;
@@ -90,51 +114,68 @@ public class HomeActivity extends Activity {
 	}
 
 	private void initActionBar() {
-		 mDrawerToggle = new ActionBarDrawerToggle(this, /* host Activity */
-				layout, /* DrawerLayout object */
-			    R.drawable.navigation_icon, /* nav drawer image to replace 'Up' caret */
-			    R.string.app_name, /* "open drawer" description for accessibility */
-			    R.string.app_name /* "close drawer" description for accessibility */
-			    ) {
-			        public void onDrawerClosed(View view) {
-			                                        // onPrepareOptionsMenu()
-			        }
+		mDrawerToggle = new ActionBarDrawerToggle(this, /* host Activity */
+		layout, /* DrawerLayout object */
+		R.drawable.navigation_icon, /* nav drawer image to replace 'Up' caret */
+		R.string.app_name, /* "open drawer" description for accessibility */
+		R.string.app_name /* "close drawer" description for accessibility */
+		) {
+			public void onDrawerClosed(View view) {
+				// onPrepareOptionsMenu()
+			}
 
-			        public void onDrawerOpened(View drawerView) {
-			                                        // onPrepareOptionsMenu()
-			        }
-			    };
-			    layout.setDrawerListener(mDrawerToggle);
-			    getActionBar().setDisplayHomeAsUpEnabled(true);
-		        getActionBar().setHomeButtonEnabled(true);
-		        
-		        ActionBar bar = getActionBar();
-		        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor(getString(R.color.orange))));
+			public void onDrawerOpened(View drawerView) {
+				// onPrepareOptionsMenu()
+			}
+		};
+		layout.setDrawerListener(mDrawerToggle);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+
+		ActionBar bar = getActionBar();
+		bar.setBackgroundDrawable(new ColorDrawable(Color
+				.parseColor(getString(R.color.orange))));
+		bar.setDisplayShowHomeEnabled(false);
 	}
-	
+
 	@Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        // Pass any configuration change to the drawer toggls
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-          return true;
-        }
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		// Pass any configuration change to the drawer toggls
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
 
-        return super.onOptionsItemSelected(item);
-    }
-	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Pass the event to ActionBarDrawerToggle, if it returns
+		// true, then it has handled the app icon touch event
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	private OnClickListener onNavigationClick() {
+		return new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if (v == btn_sell) {
+					startActivity(new Intent(mContext, ImageViewActivity.class));
+				} else {
+					Toast.makeText(mContext, "to be continue",
+							Toast.LENGTH_SHORT).show();
+				}
+			}
+		};
+	}
+
 }
