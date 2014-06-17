5ac888896f5fa3b779f11465e94784c67acae42e
tanghongphu <phutanghong@gmail.com>
2014-06-18 03:23:10 +0700

    Update chat view and title Offers

--- MainSource/3min/src/com/threemin/adapter/MessageAdapter.java
+++ MainSource/3min/src/com/threemin/adapter/MessageAdapter.java
@@ -2,14 +2,17 @@ package com.threemin.adapter;
 
 import java.util.ArrayList;
 
+import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
 import com.threemin.model.MessageModel;
 import com.threemins.R;
 
 import android.content.Context;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MessageAdapter extends BaseAdapter {
@@ -52,6 +55,13 @@ public class MessageAdapter extends BaseAdapter {
 		convertView = inflater.inflate(R.layout.layout_chat_mine, parent, false);
 		TextView tvChat = (TextView) convertView.findViewById(R.id.layout_chat_mine_tv_chat);
 		tvChat.setText(mData.get(position).getMsg());
+		
+		ImageView avatar=(ImageView) convertView.findViewById(R.id.layout_chat_mine_avatar);
+		UrlImageViewHelper.setUrlDrawable(avatar, mData.get(position).getUserModel().getFacebook_avatar());
+		
+		TextView tv_time = (TextView) convertView.findViewById(R.id.layout_chat_mine_tv_time);
+		tv_time.setText(DateUtils.getRelativeTimeSpanString(mData.get(position).getTimestamp() * 1000,
+				System.currentTimeMillis(), 0L, DateUtils.FORMAT_ABBREV_RELATIVE));
 		return convertView;
 	}
 
@@ -60,6 +70,13 @@ public class MessageAdapter extends BaseAdapter {
 		convertView = inflater.inflate(R.layout.layout_chat_theirs, parent, false);
 		TextView tvChat = (TextView) convertView.findViewById(R.id.layout_chat_theirs_tv_chat);
 		tvChat.setText(mData.get(position).getMsg());
+		
+		ImageView avatar=(ImageView) convertView.findViewById(R.id.layout_chat_theirs_avatar);
+		UrlImageViewHelper.setUrlDrawable(avatar, mData.get(position).getUserModel().getFacebook_avatar());
+		
+		TextView tv_time = (TextView) convertView.findViewById(R.id.layout_chat_theirs_tv_time_chat);
+		tv_time.setText(DateUtils.getRelativeTimeSpanString(mData.get(position).getTimestamp() * 1000,
+				System.currentTimeMillis(), 0L, DateUtils.FORMAT_ABBREV_RELATIVE));
 		return convertView;
 	}
 	
