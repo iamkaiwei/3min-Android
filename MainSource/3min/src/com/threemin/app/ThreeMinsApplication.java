bd1e25d898f93edf580c7997903a249a0f836870
TrucTran <truc.trantrung@2359media.com.vn>
2014-06-25 13:58:18 +0700

    implement push notification

---
+++ MainSource/3min/src/com/threemin/app/ThreeMinsApplication.java
@@ -0,0 +1,31 @@
+package com.threemin.app;
+
+import com.threemin.receiver.IntentReceiver;
+import com.threemins.R;
+import com.urbanairship.AirshipConfigOptions;
+import com.urbanairship.UAirship;
+import com.urbanairship.push.PushManager;
+
+import android.app.Application;
+import android.util.Log;
+
+public class ThreeMinsApplication extends Application {
+	
+	public void onCreate() {
+		
+		AirshipConfigOptions options = AirshipConfigOptions.loadDefaultOptions(this);
+		options.gcmSender = getString(R.string.gcmSender);
+	    options.transport = "gcm";
+	    options.developmentAppKey = getString(R.string.developmentAppKey);
+	    options.developmentAppSecret = getString(R.string.developmentAppSecret);
+	    options.productionAppKey = getString(R.string.productionAppKey);
+	    options.productionAppSecret = getString(R.string.productionAppSecret);
+	    options.inProduction = false;
+	    options.richPushEnabled = true;
+		UAirship.takeOff(this, options);
+		PushManager.enablePush();
+		
+		PushManager.shared().setIntentReceiver(IntentReceiver.class);
+	};
+
+}
