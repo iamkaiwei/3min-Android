db5a060027b5a6f4eda6c5bebb661f4b96ac6151
TrucTran <truc.trantrung@2359media.com.vn>
2014-06-25 17:02:11 +0700

    config Urban Airship

--- MainSource/3min/src/com/threemin/app/ThreeMinsApplication.java
+++ MainSource/3min/src/com/threemin/app/ThreeMinsApplication.java
@@ -14,14 +14,14 @@ public class ThreeMinsApplication extends Application {
 	public void onCreate() {
 		
 		AirshipConfigOptions options = AirshipConfigOptions.loadDefaultOptions(this);
-		options.gcmSender = getString(R.string.gcmSender);
-	    options.transport = "gcm";
-	    options.developmentAppKey = getString(R.string.developmentAppKey);
-	    options.developmentAppSecret = getString(R.string.developmentAppSecret);
-	    options.productionAppKey = getString(R.string.productionAppKey);
-	    options.productionAppSecret = getString(R.string.productionAppSecret);
-	    options.inProduction = false;
-	    options.richPushEnabled = true;
+//		options.gcmSender = getString(R.string.gcmSender);
+//	    options.transport = "gcm";
+//	    options.developmentAppKey = getString(R.string.developmentAppKey);
+//	    options.developmentAppSecret = getString(R.string.developmentAppSecret);
+//	    options.productionAppKey = getString(R.string.productionAppKey);
+//	    options.productionAppSecret = getString(R.string.productionAppSecret);
+//	    options.inProduction = false;
+//	    options.richPushEnabled = true;
 		UAirship.takeOff(this, options);
 		PushManager.enablePush();
 		
