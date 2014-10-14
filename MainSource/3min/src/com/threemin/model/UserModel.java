c1b1acdc4b4818c9de33c00a8c088c942ec7d65f
Truc Tran <truc.trantrung@2359media.com.vn>
2014-10-14 11:13:11 +0800

    add fields positive, negative, normal to user model

--- MainSource/3min/src/com/threemin/model/UserModel.java
+++ MainSource/3min/src/com/threemin/model/UserModel.java
@@ -4,186 +4,235 @@ import com.google.gson.annotations.SerializedName;
 
 public class UserModel {
 
-	private int id;
-	private String email;
-	@SerializedName("created_at")
-	private String createAt;
-	@SerializedName("update_at")
-	private String updateAt;
-	@SerializedName("facebook_id")
-	private String facebookId;
-	private String username;
-	@SerializedName("first_name")
-	private String firstName;
-	@SerializedName("full_name")
-	private String fullName;
-	@SerializedName("last_name")
-	private String lastName;
-	private String middleName;
-	private String gender;
-	private String birthday;
-	private String udid;
-	private String role;
-	@SerializedName("avatar")
-	private String facebook_avatar;
-	private ImageModel image;
-	@SerializedName("follower_count")
-	private int countFollowers;
-	@SerializedName("following_count")
-	private int countFollowing;
-	@SerializedName("followed")
-	private boolean isFollowed;
-	
-	public int getCountFollowers() {
-		return countFollowers;
-	}
+    private int id;
+    
+    private String email;
+    
+    @SerializedName("created_at")
+    private String createAt;
+    
+    @SerializedName("update_at")
+    private String updateAt;
+    
+    @SerializedName("facebook_id")
+    private String facebookId;
+    
+    private String username;
+    
+    @SerializedName("first_name")
+    private String firstName;
+    
+    @SerializedName("full_name")
+    private String fullName;
+    
+    @SerializedName("last_name")
+    private String lastName;
+    
+    private String middleName;
+    private String gender;
+    private String birthday;
+    private String udid;
+    private String role;
+    
+    @SerializedName("avatar")
+    private String facebook_avatar;
+    
+    private ImageModel image;
+    
+    @SerializedName("follower_count")
+    private int countFollowers;
+    
+    @SerializedName("following_count")
+    private int countFollowing;
+    
+    @SerializedName("followed")
+    private boolean isFollowed;
+    
+    @SerializedName("positive_count")
+    private int countPositive;
+    
+    @SerializedName("negative_count")
+    private int countNegative;
+    
+    @SerializedName("normal_count")
+    private int countNormal;
 
-	public void setCountFollowers(int countFollowers) {
-		this.countFollowers = countFollowers;
-	}
+    public int getCountFollowers() {
+        return countFollowers;
+    }
 
-	public int getCountFollowing() {
-		return countFollowing;
-	}
+    public void setCountFollowers(int countFollowers) {
+        this.countFollowers = countFollowers;
+    }
 
-	public void setCountFollowing(int countFollowing) {
-		this.countFollowing = countFollowing;
-	}
+    public int getCountFollowing() {
+        return countFollowing;
+    }
 
-	public boolean isFollowed() {
-		return isFollowed;
-	}
+    public void setCountFollowing(int countFollowing) {
+        this.countFollowing = countFollowing;
+    }
 
-	public void setFollowed(boolean isFollowed) {
-		this.isFollowed = isFollowed;
-	}
+    public boolean isFollowed() {
+        return isFollowed;
+    }
 
-	public ImageModel getImage() {
-		return image;
-	}
+    public void setFollowed(boolean isFollowed) {
+        this.isFollowed = isFollowed;
+    }
 
-	public void setImage(ImageModel image) {
-		this.image = image;
-	}
+    public ImageModel getImage() {
+        return image;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public void setImage(ImageModel image) {
+        this.image = image;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public String getCreateAt() {
-		return createAt;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setCreateAt(String createAt) {
-		this.createAt = createAt;
-	}
+    public String getCreateAt() {
+        return createAt;
+    }
 
-	public String getUpdateAt() {
-		return updateAt;
-	}
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt;
+    }
 
-	public void setUpdateAt(String updateAt) {
-		this.updateAt = updateAt;
-	}
+    public String getUpdateAt() {
+        return updateAt;
+    }
 
-	public String getFacebookId() {
-		return facebookId;
-	}
+    public void setUpdateAt(String updateAt) {
+        this.updateAt = updateAt;
+    }
 
-	public void setFacebookId(String facebookId) {
-		this.facebookId = facebookId;
-	}
+    public String getFacebookId() {
+        return facebookId;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public void setFacebookId(String facebookId) {
+        this.facebookId = facebookId;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public String getFullName() {
-		return fullName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
+    public String getFullName() {
+        return fullName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public String getMiddleName() {
-		return middleName;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public void setMiddleName(String middleName) {
-		this.middleName = middleName;
-	}
+    public String getMiddleName() {
+        return middleName;
+    }
 
-	public String getGender() {
-		return gender;
-	}
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
 
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
+    public String getGender() {
+        return gender;
+    }
 
-	public String getBirthday() {
-		return birthday;
-	}
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 
-	public void setBirthday(String birthday) {
-		this.birthday = birthday;
-	}
+    public String getBirthday() {
+        return birthday;
+    }
 
-	public String getUdid() {
-		return udid;
-	}
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
 
-	public void setUdid(String udid) {
-		this.udid = udid;
-	}
+    public String getUdid() {
+        return udid;
+    }
 
-	public String getRole() {
-		return role;
-	}
+    public void setUdid(String udid) {
+        this.udid = udid;
+    }
 
-	public void setRole(String role) {
-		this.role = role;
-	}
+    public String getRole() {
+        return role;
+    }
 
-	public String getFacebook_avatar() {
-		return facebook_avatar;
-	}
+    public void setRole(String role) {
+        this.role = role;
+    }
 
-	public void setFacebook_avatar(String faebook_avatar) {
-		this.facebook_avatar = faebook_avatar;
-	}
+    public String getFacebook_avatar() {
+        return facebook_avatar;
+    }
+
+    public void setFacebook_avatar(String faebook_avatar) {
+        this.facebook_avatar = faebook_avatar;
+    }
+
+    public int getCountPositive() {
+        return countPositive;
+    }
+
+    public void setCountPositive(int countPositive) {
+        this.countPositive = countPositive;
+    }
+
+    public int getCountNegative() {
+        return countNegative;
+    }
+
+    public void setCountNegative(int countNegative) {
+        this.countNegative = countNegative;
+    }
+
+    public int getCountNormal() {
+        return countNormal;
+    }
+
+    public void setCountNormal(int countNormal) {
+        this.countNormal = countNormal;
+    }
+    
+    
 
 }
