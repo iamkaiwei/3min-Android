38ebb6343d32c5480e7b7e2483be2c3e7bf66da0
Truc Tran <truc.trantrung@2359media.com.vn>
2014-10-22 17:37:46 +0700

    add test code

--- MainSource/3min/src/com/threemin/app/ThreeMinsBaseActivity.java
+++ MainSource/3min/src/com/threemin/app/ThreeMinsBaseActivity.java
@@ -5,24 +5,40 @@ import android.support.v4.app.FragmentActivity;
 import me.imid.swipebacklayout.lib.SwipeBackLayout;
 import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
 
-//public class ThreeMinsBaseActivity extends FragmentActivity {
-public class ThreeMinsBaseActivity extends SwipeBackActivity {
+//public class ThreeMinsBaseActivity extends SwipeBackActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        getSwipeBackLayout().setEdgeTrackingEnabled(SwipeBackLayout.EDGE_LEFT);
+//        
+//    }
+//    
+//    @Override
+//    public void onBackPressed() {
+//        super.onBackPressed();
+//        scrollToFinishActivity();
+//    }
+//    
+//    public void disableSwipeBack() {
+//        getSwipeBackLayout().setEnableGesture(false);
+//    }
+//    
+//}
+
+public class ThreeMinsBaseActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSwipeBackLayout().setEdgeTrackingEnabled(SwipeBackLayout.EDGE_LEFT);
-        
     }
     
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        scrollToFinishActivity();
     }
     
     public void disableSwipeBack() {
-        getSwipeBackLayout().setEnableGesture(false);
     }
     
 }
