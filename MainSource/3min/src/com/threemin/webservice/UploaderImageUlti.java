b34fecef15dcdefea585f17243b06fc1f436865c
Truc Tran <truc.trantrung@2359media.com.vn>
2014-11-13 15:29:12 +0700

    refactor

--- MainSource/3min/src/com/threemin/webservice/UploaderImageUlti.java
+++ MainSource/3min/src/com/threemin/webservice/UploaderImageUlti.java
@@ -33,45 +33,45 @@ import com.threemin.model.ProductModel;
 
 public class UploaderImageUlti {
     private final String tag = "UploaderImageUlti";
-    
-	private DefaultHttpClient mHttpClient;
-	private final int TIMEOUT = 60000;
 
-	public UploaderImageUlti() {
-		HttpParams params = new BasicHttpParams();
-		params.setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
-		mHttpClient = new DefaultHttpClient(params);
-	}
+    private DefaultHttpClient mHttpClient;
+    private final int TIMEOUT = 60000;
 
-	public HttpResponse uploadUserPhoto(String url, ProductModel model, String tokken) {
+    public UploaderImageUlti() {
+        HttpParams params = new BasicHttpParams();
+        params.setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+        mHttpClient = new DefaultHttpClient(params);
+    }
 
-		try {
+    public HttpResponse uploadUserPhoto(String url, ProductModel model, String tokken) {
 
-			HttpPost httppost = new HttpPost(url);
-	         MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        try {
 
-	            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+            HttpPost httppost = new HttpPost(url);
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
 
-	            /* example for adding an image part */
-	            for (ImageModel imageModel : model.getImages()) {
-	                ContentType contentType = ContentType.create("image/jpeg");
-	                File imageFile = new File(imageModel.getUrl());
-	                FileBody fileBody = new FileBody(imageFile, contentType, imageFile.getName());
-	                builder.addPart("images[]", fileBody);
-	            }
-	            addProductInfo(builder, tokken, model);
-	            
-			httppost.setEntity(builder.build());
-			return mHttpClient.execute(httppost);
-		} catch (Exception e) {
-			e.printStackTrace();
-			Log.i("UploaderImageUlti", e.toString());
-			return null;
-		}
-	}
-	
-	public void addProductInfo(MultipartEntityBuilder builder, String tokken, ProductModel model) {
-	    if (!TextUtils.isEmpty(model.getDescription())) {
+            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+
+            /* example for adding an image part */
+            for (ImageModel imageModel : model.getImages()) {
+                ContentType contentType = ContentType.create("image/jpeg");
+                File imageFile = new File(imageModel.getUrl());
+                FileBody fileBody = new FileBody(imageFile, contentType, imageFile.getName());
+                builder.addPart("images[]", fileBody);
+            }
+            addProductInfo(builder, tokken, model);
+
+            httppost.setEntity(builder.build());
+            return mHttpClient.execute(httppost);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i("UploaderImageUlti", e.toString());
+            return null;
+        }
+    }
+
+    public void addProductInfo(MultipartEntityBuilder builder, String tokken, ProductModel model) {
+        if (!TextUtils.isEmpty(model.getDescription())) {
             builder.addTextBody("description", model.getDescription());
         }
         builder.addTextBody("access_token", tokken);
@@ -85,16 +85,16 @@ public class UploaderImageUlti {
             builder.addTextBody("venue_long", model.getVenueLong() + "");
             builder.addTextBody("venue_lat", model.getVenueLat() + "");
         }
-	}
-	
-	public ProductModel updateUserPhoto(String url, ProductModel model, String tokken) {
+    }
+
+    public ProductModel updateUserPhoto(String url, ProductModel model, String tokken) {
 
         try {
 
             HttpPut httpPut = new HttpPut(url);
             MultipartEntityBuilder builder = createMultipartEntityBuilder(model, tokken);
             httpPut.setEntity(builder.build());
-            
+
             HttpResponse response = mHttpClient.execute(httpPut);
             if (response != null) {
                 HttpEntity r_entity = response.getEntity();
@@ -118,17 +118,17 @@ public class UploaderImageUlti {
                 }
             }
             return null;
-            
+
         } catch (Exception e) {
             e.printStackTrace();
             Log.i(tag, e.toString());
             return null;
         }
     }
-	
-	//use to update product
-	public MultipartEntityBuilder createMultipartEntityBuilder(ProductModel model, String tokken) {
-	    MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+
+    // use to update product
+    public MultipartEntityBuilder createMultipartEntityBuilder(ProductModel model, String tokken) {
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
 
         builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
 
@@ -142,7 +142,7 @@ public class UploaderImageUlti {
             } else if (type == ImageModel.TYPE_EDIT_PRODUCT_NO_CHANGE) {
                 continue;
             }
-            
+
             if (type == ImageModel.TYPE_EDIT_PRODUCT_DELETE) {
                 builder.addTextBody("images[" + index + "][_destroy]", "1");
             } else {
@@ -151,11 +151,11 @@ public class UploaderImageUlti {
                 FileBody fileBody = new FileBody(imageFile, contentType, imageFile.getName());
                 builder.addPart("images[" + index + "][content]", fileBody);
             }
-            
+
             index++;
         }
         addProductInfo(builder, tokken, model);
         return builder;
-	}
+    }
 
 }
